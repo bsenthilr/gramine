a36f6985242482e8436aa8fd234ed1c7a9d6e5d9
Balakrishnan, Senthil <senthil.balakrishnan@intel.com>
2024-01-19 15:35:54 +0000

    fixup! poc test
    dump for debug

--- CI-Examples/dekTest/dek.cpp
+++ CI-Examples/dekTest/dek.cpp
@@ -3,9 +3,24 @@
 #include <fstream>
 #include <iostream>
 #include <random>
+#include <filesystem>
 
-auto dek = "/key/dek";
-auto dataSealPath = "/dev/attestation/keys/dek";
+const std::string dek = "/key/dek";
+const std::string dataSealKeyPath = "/dev/attestation/keys/dek";
+const std::string dataPath = "/data";
+
+void dumpFile(const std::filesystem::path &path) {
+    std::cout << "file: " << path << std::endl;
+    std::ifstream ifs;
+    ifs.open(path, std::ifstream::in);
+    char c = ifs.get();
+    while (ifs.good()) {
+        std::cout << c << " ";
+        c = ifs.get();
+    }
+    std::cout << std::endl;
+    ifs.close();
+}
 
 // DEK sealed by mrsigner
 void generateDEK() {
@@ -20,42 +35,37 @@ void generateDEK() {
     std::cout << "generating secret file...\n";
     std::ofstream ofs, attestKeyFs;
     ofs.open(dek, std::ofstream::out);
-    attestKeyFs.open(dataSealPath, std::ofstream::out);
+    attestKeyFs.open(dataSealKeyPath, std::ofstream::out);
     for (auto i : data) {
-        //std::cout << i << " ";
+        std::cout << i << " ";
         ofs << i;
         attestKeyFs << i;
     }
     //std::cout << std::endl;
     ofs.close();
     attestKeyFs.close();
+
+    // ensure the attestation key path exists
+    const std::filesystem::path  fsPath{dataSealKeyPath};
+    //std::cout << fsPath << std::endl;
+    std::cout << fsPath << " exist: " << std::filesystem::exists(fsPath) << std::endl;
+    dumpFile(fsPath);
 }
 
 // create data
 void generateData() {
   std::ofstream dataFs;
-  dataFs.open("/data/trialdata.txt", std::ofstream::out);
+  const std::filesystem::path fsPath{dataPath + "/trialdata.txt"};
+  dataFs.open(fsPath, std::ofstream::out);
   dataFs << "dsafkj;sdjfl;asjdfsadlfjsadljfl;asd" << std::endl;
   dataFs.close();
+  dumpFile(fsPath);
 }
 
 int main(void) {
     std::cout << "user provided dek...\n";
-    // std::cout << "Read secret file: " << secret << std::endl;
-    // std::ifstream ifs;
-    // ifs.open(secret, std::ifstream::in);
-    // char c = ifs.get();
-    // while (ifs.good()) {
-    //     std::cout << c << " ";
-    //     c = ifs.get();
-    // }
-    // std::cout << std::endl;
-    // ifs.close();
-
     generateDEK();
-
     // Toss data to data and see if its encrypted..
     generateData();
-
     return 0;
 }
